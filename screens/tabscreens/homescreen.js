f4a687639176431750c8828a7d743c990ef929a5
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-08-11 01:42:54 +0100

    details added

--- screens/tabscreens/homescreen.js
+++ screens/tabscreens/homescreen.js
@@ -2,8 +2,13 @@ import { Text, View } from "react-native";
 
 function HomeScreen() {
   return (
-    <View>
-      <Text> this is home screen</Text>
+    <View style={{ padding: 20 }}>
+      <Text>Details: 2024 Lexus NX • 350</Text>
+      <Text>Miles: 8,615</Text>
+      <Text>Price: $10,000</Text>
+      <Text>Deposite: $1,000</Text>
+      <Text>Available</Text>
+      <Text>warranty: "2 Years</Text>
     </View>
   );
 }
